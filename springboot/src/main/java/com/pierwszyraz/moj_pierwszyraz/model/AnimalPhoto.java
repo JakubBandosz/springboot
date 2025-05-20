2741f726b8f39a266878d8109dbda596edafae6a
JakubBandosz <jakubbandosz11@gmail.com>
2025-05-20 13:07:50 +0200

    Update AnimalPhoto.java

--- springboot/src/main/java/com/pierwszyraz/moj_pierwszyraz/model/AnimalPhoto.java
+++ springboot/src/main/java/com/pierwszyraz/moj_pierwszyraz/model/AnimalPhoto.java
@@ -9,6 +9,8 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 
 
 @Entity
