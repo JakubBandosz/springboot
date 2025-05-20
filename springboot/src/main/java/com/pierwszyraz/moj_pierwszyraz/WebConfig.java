ccf265c37df40e2674a9ba71e49e4a3c394ab1d7
JakubBandosz <jakubbandosz11@gmail.com>
2025-05-21 00:14:55 +0200

    Update WebConfig.java

--- springboot/src/main/java/com/pierwszyraz/moj_pierwszyraz/WebConfig.java
+++ springboot/src/main/java/com/pierwszyraz/moj_pierwszyraz/WebConfig.java
@@ -1,8 +1,9 @@
+package com.pierwszyraz.moj_pierwszyraz;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class WebConfig {
 
